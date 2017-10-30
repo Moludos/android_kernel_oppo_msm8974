9185e7cb936fb26a3e9b4bee3bc80ec6f68ecb89
Kshitij Gupta <kshitijgm@gmail.com>
2017-10-30 12:16:43 +0000

    touchscreen: fire a small vibration on dt2w
    - Based on https://github.com/franciscofranco/Shamu/commit/ec81afb3f0c0eed62b3dfd0404731d2898b245fc
    - Adapt change for bacon:
      * vib_dev is a static struct that can be used after
         qpnp_vibrator_probe finishes
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    
    Conflicts:
    	drivers/input/touchscreen/synaptics_dsx_i2c.c

--- drivers/input/touchscreen/synaptics_dsx_i2c.c
+++ drivers/input/touchscreen/synaptics_dsx_i2c.c
@@ -33,6 +33,7 @@
 #include <asm/uaccess.h>
 #include <mach/device_info.h>
 #include <linux/pcb_version.h>
+#include <linux/qpnp/vibrator.h>
 
 #include "synaptics_dsx.h"
 #include "synaptics_dsx_i2c.h"
@@ -99,9 +100,7 @@ char *tp_firmware_strings[TP_TYPE_MAX][LCD_TYPE_MAX] = {
 #define NO_SLEEP_ON (1 << 2)
 #define CONFIGURED (1 << 7)
 
-#ifdef CONFIG_MACH_N3
-static atomic_t key_is_touched;
-#endif
+#define VIBRATE_STRENGTH 27
 
 static int synaptics_rmi4_i2c_read(struct synaptics_rmi4_data *rmi4_data,
 		unsigned short addr, unsigned char *data,
@@ -2469,7 +2468,10 @@ static unsigned char synaptics_rmi4_update_gesture2(unsigned char *gesture,
 		case SYNA_ONE_FINGER_DOUBLE_TAP:
 			gesturemode = DouTap;
 			if (atomic_read(&syna_rmi4_data->double_tap_enable))
+			{
 				keyvalue = KEY_WAKEUP;
+				vibrate(VIBRATE_STRENGTH);
+			}
 			break;
 
 		case SYNA_ONE_FINGER_DIRECTION:
